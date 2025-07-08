8710400c82f2f21f5f85221905baa4db7804d0f0
cotmdgns <qmflskzm@gmail.com>
2025-07-08 22:41:07 +0900

    2025-07-08

--- codingText/src/codingTextLevel01/Level04.java
+++ codingText/src/codingTextLevel01/Level04.java
@@ -1,27 +1,35 @@
 package codingTextLevel01;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Level04 {
 
 	public static void main(String[] args) {
 		
 		Level04 aa = new Level04();
 		
-		int[][]	a = {{20,40},{20,60},{70,50},{80,80},{70,10},{80,70}};
-		System.out.println(aa.solution(a));
+		int[][]	a = {{1, 3},{3, 1},{2, 3},{3, 2},{1, 2},{1, 1}};
+		System.out.println(Arrays.toString(aa.solution(a)));
 	}
 
     public int[] solution(int[][] score) {
-    	// 반복문을 돌려서 i번째로 들어갈꺼임
-    	// 근데 조건문을 걸어서 i번째랑 그 뒤에오는 i번째의 숫자가 같으면 i-1해서 이걸 집어 넣으면 됨
-    	// 어쩌피 첫번째랑 마지막번째는 안걸림 왜? 조건문이 false이기때문에 그냥 넘어가게 될 로직임
-    	// 문제는 이제 평균을 구하고 나온 배열 위치에 i를 넣어야 하는데[
-    	// 정렬안 안한 상태에서 비교를 해야함
-    	// 아니면 정렬 후, 정렬 한 배열들을 score 평균이랑 비교 해서 넣기?
-    
-        int[] answer = {};
+        Double[] avg = new Double[score.length];
         for(int i=0;i<score.length;i++	) {
-        	
+        	avg[i] = ((double) ( score[i][0] + score[i][1] ) / 2);
         }
-        return answer;
+        int [] sum = new int[score.length];
+        for(int i=0;i<avg.length;i++) {
+        	for(int j=0;j<avg.length;j++) {
+        		if(avg[i] < avg[j]) {
+        			sum[i]++ ;
+        		}
+        	}
+        	sum[i]++;
+        }
+        return sum;
     }
 }
+;
